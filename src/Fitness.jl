1bc0335aa536e73767a165ea37268f9644c335fb
Fabian Beez <desjardinslegedz@icloud.com>
2024-12-06 13:20:17 +0100

    rosenbrock fitness

--- src/Fitness.jl
+++ src/Fitness.jl
@@ -2,6 +2,19 @@ module Fitness
 
 using ..Types
 
-# TODO
+function rosenbrock_fitness(values::Vector{Float64})::Float64
+    m = length(values)
+    if m < 2
+        throw(ArgumentError("Rosenbrock function requires at least two dimensions."))
+    end
+    
+     # negative to maximize the function
+    return - sum(
+        100 * (values[i+1] - values[i]^2)^2 + (1 - values[i])^2
+        for i in 1:m-1
+    )
+end
+
+export rosenbrock_fitness
 
 end
