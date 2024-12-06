aff0c1990d3536eaa86289485b1f90afade796b9
Fabian Beez <desjardinslegedz@icloud.com>
2024-12-06 14:43:33 +0100

    WIP with some problems

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -2,4 +2,13 @@ using Test
 
 include("../src/GeneticAlgorithms.jl")
 
-# TODO
+using Plots
+using GeneticAlgorithms.Utils
+using GeneticAlgorithms.Fitness
+
+@testset "Utils Visualization Tests" begin
+    # f(x, y) = rosenbrock_fitness([x, y])
+    # points = [(1.0, 0.0), (-1.0, -1.0)]
+    # plt = visualize_function_with_contours(f, x_range=(-2.0, 2.0), y_range=(-1.0, 3.0), points=points)
+    # savefig(plt, "contour_plot.png")
+end
