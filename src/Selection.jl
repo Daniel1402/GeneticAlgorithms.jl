6e1a38ed961c58f2753e714961d5f0d1f3d832a2
Jonas <jonas@mennicke.com>
2024-12-07 18:59:30 +0100

    add: rouletteWheelSelection

--- src/Selection.jl
+++ src/Selection.jl
@@ -2,6 +2,32 @@ module Selection
 
 using ..Types
 
-# TODO
+function rouletteWheelSelection(population::Vector{Vector{Int64}}, fitness_scores::Vector{Float64}, rand_generator::Function = rand)::Tuple{Vector{Int64}, Vector{Int64}}
+    if length(population) != length(fitness_scores)
+        throw(ArgumentError("Population and fitness scores must have the same length"))
+    end
 
+    # Helper function to select an index based on the fitness scores
+    function indexSelection(fitness_scores, rand_generator)
+        # Calculate the cumulative probabilities
+        cum_probs = cumsum(fitness_scores ./ sum(fitness_scores))
+
+        selected_index = findfirst(cum_prob -> rand_generator() <= cum_prob, cum_probs)
+
+        return selected_index == nothing ? length(fitness_scores) : selected_index
+    end
+
+    p1_index = indexSelection(fitness_scores, rand_generator)
+
+    # Filter out the selected index & search for the second parent
+    p2_index = indexSelection(fitness_scores[1:end .!= p1_index], rand_generator)
+
+    # Adjust the index to account for the removed element
+    p2_index += (p2_index >= p1_index ? 1 : 0)
+
+    return population[p1_index], population[p2_index]
+end
+
+
+export rouletteWheelSelection   
 end
