c4bdf34c7a16b328f5c782ccf56b1094d38e9255
Dominik S. <dsiebelt@campus.tu-berlin.de>
2024-12-08 17:48:03 +0100

    Add call function to mutation type

--- src/Mutation.jl
+++ src/Mutation.jl
@@ -3,8 +3,8 @@ module Mutation
 using ..Types
 
 struct RealGeneMutation{T<:Real}
-    mutation_rate::Float64 = 0.1
-    mutation_interval::Tuple{T, T} = (-0.5, 0.5)
+    mutation_rate::Float64
+    mutation_interval::Tuple{T, T}
 
     function RealGeneMutation(mutation_rate::Float64, mutation_interval::Tuple{T, T}) where T<:Real
         if mutation_rate < 0 || mutation_rate > 1
@@ -16,13 +16,17 @@ struct RealGeneMutation{T<:Real}
         if mutation_interval[1] == mutation_interval[2]
             throw(ArgumentError("Mutation interval must have a range"))
         end
-        new(mutation_rate, mutation_interval)
+        new{eltype(mutation_interval)}(mutation_rate, mutation_interval)
     end
 
 end
 
-function (c::RealGeneMutation)(genes::AbractVector{Real})::AbstractVector{Real}
+function (c::RealGeneMutation)(genes::Vector{<:Real})::Vector{<:Real}
+    """
+    Mutates the genes with a probability of c.mutation_rate and values in the interval c.mutation_interval
+    """
     return [gene + (rand() < c.mutation_rate ? rand(c.mutation_interval) : 0) for gene in genes]
 end
 
+export RealGeneMutation
 end
--- test/test_mutation.jl
+++ test/test_mutation.jl
@@ -1,4 +1,17 @@
 using Test
+using GeneticAlgorithms.Mutation
 
-using GeneticAlgorithms
-# TODO
+
+@testset "Mutation.jl" begin
+
+    mutation = RealGeneMutation(0.1, (-0.5, 0.5))
+    mutated_genes = mutation([0.0, 0.0, 0.0, 0.0, 0.0])
+    @test length(mutated_genes) == 5
+    for gene in mutated_genes
+        @test -0.5 <= gene <= 0.5
+    end
+    @test mutation.mutation_rate == 0.1
+    @test mutation.mutation_interval == (-0.5, 0.5)
+end
+
+
