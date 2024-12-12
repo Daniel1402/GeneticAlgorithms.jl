058bfa0b84eef5fb09ac377299a919796bf0f57b
Daniel Kirste <daniel.kirste@stud.tu-darmstadt.de>
2024-12-12 13:08:20 +0100

    Small fixes

--- src/Mutation.jl
+++ src/Mutation.jl
@@ -4,15 +4,15 @@ using ..Types
 using Distributions
 
 """
-    Defines the mutation of genes of real values (currently only Float64 and Integer types (including Bool))
-    Mutation is applied for each gene on its own with probability `mutation_rate` vith values from the uniform
-    distribution in the interval 'mutation_interval'.
+    Defines the mutation of genes of real values (currently only `Float64` and `Integer` types (including `Bool`))
+    Mutation is applied for each gene on its own with probability `mutation_rate` with values from the uniform
+    distribution in the interval `mutation_interval`.
 """
 struct RealGeneMutation{T<:Union{Float64, Integer}}
     mutation_rate::Float64
     mutation_interval::Tuple{T, T}
 
-    function RealGeneMutation(mutation_rate::Float64, mutation_interval::Tuple{T, T}) where T<:Real
+    function RealGeneMutation(mutation_rate::Float64, mutation_interval::Tuple{T, T}) where T<:Union{Float64, Integer}
         if mutation_rate < 0 || mutation_rate > 1
             throw(ArgumentError("Mutation rate must be between 0 and 1"))
         end
@@ -28,29 +28,29 @@ struct RealGeneMutation{T<:Union{Float64, Integer}}
 end
 
 """
-    Mutates the genes with a probability of c.mutation_rate and values in the interval c.mutation_interval
+    Mutates the genes with a probability of c.mutation_rate and values in the interval `c.mutation_interval`.
 """
 function (c::RealGeneMutation)(genes::Matrix{T})::Matrix{T} where T<:Float64
-    
-    mask = rand((0,1), size(genes)) .< c.mutation_rate
+    Mutation.RealGeneMutation(0.1, ("a", "b"))
+    mask = rand(Uniform(0, 1), size(genes)) .< c.mutation_rate
     random_additions = rand(Uniform(c.mutation_interval[1], c.mutation_interval[2]), size(genes))
     return genes .+ (mask .&& random_additions)
 end
 
 """
-    Mutates the genes with a probability of c.mutation_rate and values in the interval c.mutation_interval
+    Mutates the genes with a probability of c.mutation_rate and values in the interval `c.mutation_interval`.
 """
 function (c::RealGeneMutation)(genes::Matrix{T})::Matrix{T} where T<:Integer
-    mask = rand((0,1), size(genes)) .< c.mutation_rate
+    mask = rand(Uniform(0,1), size(genes)) .< c.mutation_rate
     random_additions = rand(range(c.mutation_interval[1], c.mutation_interval[2]), size(genes))
     return genes .+ (mask .&& random_additions)
 end
 
 """
-    Mutates the genes with a probability of c.mutation_rate and values in the interval c.mutation_interval
+    Mutates the genes with a probability of c.mutation_rate and values in the interval `c.mutation_interval`.
 """
 function (c::RealGeneMutation)(genes::Matrix{T})::Matrix{T} where T<:Bool
-    mask = rand((0,1), size(genes)) .< c.mutation_rate
+    mask = rand(Uniform(0,1), size(genes)) .< c.mutation_rate
     return genes .⊻ mask # bitwise XOR
 end
 
