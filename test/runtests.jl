1254c070c0a2765f47d89ca6ae63795d23e81509
Dominik S. <dsiebelt@campus.tu-berlin.de>
2024-12-13 16:45:16 +0100

    Fix Selection Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,9 @@ using Test
 #include("../src/GeneticAlgorithms.jl")
 using GeneticAlgorithms
 
-include("test_crossover.jl")
-include("test_population_initialization.jl")
-# include("test_selection.jl")
+# include("test_crossover.jl")
+# include("test_population_initialization.jl")
+include("test_selection.jl")
 # include("test_mutation.jl")
 # include("test_fitness.jl")
 # include("test_genetic_algorithms.jl")
