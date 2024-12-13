cf04eb8995d031c94fc0269444841801a73b1157
Daniel Kirste <daniel.kirste@stud.tu-darmstadt.de>
2024-12-13 12:53:30 +0100

    Add struct for initialization

--- src/PopulationInitialization.jl
+++ src/PopulationInitialization.jl
@@ -2,26 +2,41 @@ module PopulationInitialization
 
 using Distributions
 
+abstract type InitializationMethod end
+
+struct RealUniformInitialization{T<:Real} <: InitializationMethod 
+    population_size::Int64
+    chromosome_size::Int64
+    interval::Tuple{T, T}
+    
+    function RealUniformInitialization(population_size::Int64, chromosome_size::Int64, interval::Tuple{T, T}) where T <: Real
+        if interval[1] >= interval[2]
+            throw(ArgumentError("Upper bound must be greater than lower bound of the interval."))
+        end
+        if population_size <= 0
+            throw(ArgumentError("Population size must be greater zero."))
+        end
+        if chromosome_size <= 0
+            throw(ArgumentError("Chromosome size must be greater zero."))
+        end
+        new{eltype(interval)}(population_size, chromosome_size, interval)
+    end
+end
 """
     uniform(population_size, chromosome_size, interval)
 
 Creates a population of `population_size` including chromosomes of `chromosome_size`. 
 The chromosome-values are drawn from a uniform distribution over `interval`.
 """
-function uniform(population_size::Int64, chromosome_size::Int64, interval=(0, 1))::Matrix{Float64}
-    if interval[1] >= interval[2]
-        throw(ArgumentError("Upper bound must be greater than lower bound of the interval."))
-    end
-    if population_size <= 0
-        throw(ArgumentError("Population size must be greater zero."))
-    end
-    if chromosome_size <= 0
-        throw(ArgumentError("Chromosome size must be greater zero."))
-    end
-
+function (c::UniformInitialization{T})()::Population{Float64Chromosome} where T <: Float64
     return rand(Uniform(interval[1], interval[2]), (population_size, chromosome_size))
 end
 
-export uniform
+function (c::UniformInitialization{T})()::Population{IntegerChromosome} where T <: Integer
+    return rand(interval[1]:interval[2], (population_size, chromosome_size))
+end
+
+
+export RealUniformInitialization
 
 end
