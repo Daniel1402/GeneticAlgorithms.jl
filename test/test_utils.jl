58ca885ed164a651d11464e1be4fd66443b28959
Jonas <jonas@mennicke.com>
2024-12-15 18:38:04 +0100

    fix test path

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -9,11 +9,11 @@ using GeneticAlgorithms.Fitness
     f(x, y) = sin(x) * cos(y)
     points = [(-π/2, 0.0), (π/2, 0.0)]
     plt = visualize_function_with_contours(f, x_range=(-1π, 1π), y_range=(-1π, 1π), points=points)
-    savefig(plt, "plot_results_test/sin*cos.png")
+    savefig(plt, "../plot_results_test/sin*cos.png")
 
     # Rosenbrock
     f(x, y) = rosenbrock_fitness([x, y])
     points = [(1.0, 1.0)]
     plt = visualize_function_with_contours(f, x_range=(-2.0, 2.0), y_range=(-1.0, 3.0), points=points)
-    savefig(plt, "plot_results_test/rosenbrock.png")
+    savefig(plt, "../plot_results_test/rosenbrock.png")
 end
