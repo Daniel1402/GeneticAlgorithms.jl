90ad742f98bd388e5f44a57cca0e682d20db939f
Jonas <jonas@mennicke.com>
2024-12-15 18:45:28 +0100

    fix path

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -5,15 +5,21 @@ using GeneticAlgorithms.Utils
 using GeneticAlgorithms.Fitness
 
 @testset "Visualization Tests" begin
-    # Sample test
-    f(x, y) = sin(x) * cos(y)
-    points = [(-π/2, 0.0), (π/2, 0.0)]
-    plt = visualize_function_with_contours(f, x_range=(-1π, 1π), y_range=(-1π, 1π), points=points)
-    savefig(plt, "../plot_results_test/sin*cos.png")
+    try
+        # Sample test
+        f(x, y) = sin(x) * cos(y)
+        points = [(-π/2, 0.0), (π/2, 0.0)]
+        plt = visualize_function_with_contours(f, x_range=(-1π, 1π), y_range=(-1π, 1π), points=points)
+        savefig(plt, "sin*cos.png")
 
-    # Rosenbrock
-    f(x, y) = rosenbrock_fitness([x, y])
-    points = [(1.0, 1.0)]
-    plt = visualize_function_with_contours(f, x_range=(-2.0, 2.0), y_range=(-1.0, 3.0), points=points)
-    savefig(plt, "../plot_results_test/rosenbrock.png")
+        # Rosenbrock
+        f(x, y) = rosenbrock_fitness([x, y])
+        points = [(1.0, 1.0)]
+        plt = visualize_function_with_contours(f, x_range=(-2.0, 2.0), y_range=(-1.0, 3.0), points=points)
+        savefig(plt, "rosenbrock.png")
+        @test true
+    catch e
+        @error "Test failed with error" exception=e
+        @test false
+    end
 end
