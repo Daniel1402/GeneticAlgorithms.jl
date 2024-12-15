de9e3c173a7c29c2ebd71b761bcfe72a0c82aa5c
Fabian Beez <desjardinslegedz@icloud.com>
2024-12-15 17:02:32 +0100

    function visualization

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,5 +1,30 @@
 module Utils
 
-# TODO
+using Plots
+
+gr() # set gr backend
+
+function visualize_function_with_contours(
+    f::Function;
+    x_range::Tuple{Float64, Float64} = (-2.0, 2.0),
+    y_range::Tuple{Float64, Float64} = (-2.0, 2.0),
+    points::Vector{Tuple{Float64, Float64}} = []
+)
+    # generate function grid
+    x = range(x_range[1], x_range[2], length=100)
+    y = range(y_range[1], y_range[2], length=100)
+    z = [f(xi, yi) for yi in y, xi in x]
+
+    plt = contour(x, y, z, color=:viridis, linewidth=2)
+
+    # add points
+    if !isempty(points)
+        scatter!(plt, [p[1] for p in points], [p[2] for p in points], color=:red, marker=:circle, label="provided points")
+    end
+
+    return plt
+end
+
+export visualize_function_with_contours
 
 end
--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,4 +1,19 @@
 using Test
+using Plots
 
 using GeneticAlgorithms.Utils
-# TODO
+using GeneticAlgorithms.Fitness
+
+@testset "Utils Visualization Tests" begin
+    # Sample test
+    f(x, y) = sin(x) * cos(y)
+    points = [(-π/2, 0.0), (π/2, 0.0)]
+    plt = visualize_function_with_contours(f, x_range=(-1π, 1π), y_range=(-1π, 1π), points=points)
+    savefig(plt, "sin*cos.png")
+
+    # Rosenbrock
+    f(x, y) = rosenbrock_fitness([x, y])
+    points = [(1.0, 1.0)]
+    plt = visualize_function_with_contours(f, x_range=(-2.0, 2.0), y_range=(-1.0, 3.0), points=points)
+    savefig(plt, "rosenbrock.png")
+end
