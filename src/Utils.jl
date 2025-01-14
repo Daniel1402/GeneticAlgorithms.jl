84bad8ba8f862cf429fd0f23cd8d804ff72da72d
Fabian Beez <desjardinslegedz@icloud.com>
2025-01-14 18:47:37 +0100

    optimization path + fix visualization bug

--- src/Utils.jl
+++ src/Utils.jl
@@ -22,18 +22,19 @@ function visualize_function_with_contours(
     y_range::Tuple{Float64, Float64} = (-2.0, 2.0),
     points::Vector{Tuple{Float64, Float64}} = []
 )
-    # generate function grid
     x = range(x_range[1], x_range[2], length=100)
     y = range(y_range[1], y_range[2], length=100)
     z = [f(xi, yi) for yi in y, xi in x]
 
     plt = contour(x, y, z, color=:viridis, linewidth=2)
 
-    # add points
     if !isempty(points)
-        indices = 1:length(points)  # Indices of the points
-        colors = cgrad(:RdBu, length(x)).colors  # Generate a colormap
-        scatter!(plt, [p[1] for p in points], [p[2] for p in points], color=colors[indices], marker=:circle, label="provided points")
+        x_points = [p[1] for p in points]
+        y_points = [p[2] for p in points]
+
+        plot!(plt, x_points, y_points, color=:red, label="optimization path", linewidth=2)
+        scatter!(plt, x_points[2:end], y_points[2:end], color=:red, marker=:circle, markersize=3, label="")
+        scatter!(plt, [x_points[end]], [y_points[end]], color=:green, marker=:circle, markersize=5, label="optimization result")
     end
 
     return plt
