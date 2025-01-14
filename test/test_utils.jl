831526da766892bb6cb35b071dcd7f60e410d196
Fabian Beez <desjardinslegedz@icloud.com>
2025-01-14 19:28:03 +0100

    fix tests

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -8,14 +8,14 @@ using GeneticAlgorithms.Fitness
     try
         # Sample test
         f(x, y) = sin(x) * cos(y)
-        points = [(-π/2, 0.0), (π/2, 0.0)]
-        plt = visualize_function_with_contours(f, x_range=(-1π, 1π), y_range=(-1π, 1π), points=points)
+        path = [(-π/2, 0.0), (π/2, 0.0)]
+        plt = visualize_function_with_contours(f, x_range=(-1π, 1π), y_range=(-1π, 1π), path=path)
         savefig(plt, "sin*cos.png")
 
         # Rosenbrock
         f(x, y) = rosenbrock_fitness([x, y])
-        points = [(1.0, 1.0)]
-        plt = visualize_function_with_contours(f, x_range=(-2.0, 2.0), y_range=(-1.0, 3.0), points=points)
+        path = [(1.0, 1.0)]
+        plt = visualize_function_with_contours(f, x_range=(-2.0, 2.0), y_range=(-1.0, 3.0), path=path)
         savefig(plt, "rosenbrock.png")
         @test true
     catch e
