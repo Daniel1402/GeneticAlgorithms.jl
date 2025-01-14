079f520728d357571fde588869a75a96bab64f5a
Fabian Beez <desjardinslegedz@icloud.com>
2025-01-14 19:54:08 +0100

    support asymmetric initializations

--- src/PopulationInitialization.jl
+++ src/PopulationInitialization.jl
@@ -14,29 +14,39 @@ The type is determined by the `interval`.
 struct RealUniformInitialization{T<:Real} <: PopulationInitializationMethod
     population_size::Int64
     chromosome_size::Int64
-    interval::Tuple{T,T}
+    intervals::Vector{Tuple{T,T}}
 
-    function RealUniformInitialization(population_size::Int64, chromosome_size::Int64, interval::Tuple{T,T}) where {T<:Real}
-        if interval[1] >= interval[2]
-            throw(ArgumentError("Upper bound must be greater than lower bound of the interval."))
+    function RealUniformInitialization(population_size::Int64, chromosome_size::Int64, intervals::Union{Tuple{T,T}, Vector{Tuple{T,T}}}) where {T<:Real}
+        if typeof(intervals) <: Tuple
+            intervals = fill(intervals, chromosome_size)
+        elseif length(intervals) != chromosome_size
+            throw(ArgumentError("Number of intervals must match the chromosome size."))
+        end
+
+        for interval in intervals
+            if interval[1] >= interval[2]
+                throw(ArgumentError("Upper bound must be greater than lower bound of the interval."))
+            end
         end
         if population_size <= 0
-            throw(ArgumentError("Population size must be greater zero."))
+            throw(ArgumentError("Population size must be greater than zero."))
         end
         if chromosome_size <= 0
-            throw(ArgumentError("Chromosome size must be greater zero."))
+            throw(ArgumentError("Chromosome size must be greater than zero."))
         end
-        new{eltype(interval)}(population_size, chromosome_size, interval)
+
+        new{eltype(intervals[1])}(population_size, chromosome_size, intervals)
     end
 end
 
 function (c::RealUniformInitialization{T})()::Population{Float64Chromosome} where {T<:Float64}
-    return Population([Float64Chromosome(rand(Uniform(c.interval[1], c.interval[2]), c.chromosome_size)) for _ in 1:c.population_size])
+    return Population([Float64Chromosome([rand(Uniform(c.intervals[i][1], c.intervals[i][2])) for i in 1:c.chromosome_size]) for _ in 1:c.population_size])
 end
 
 function (c::RealUniformInitialization{T})()::Population{IntegerChromosome} where {T<:Integer}
-    return Population([IntegerChromosome(rand((c.interval[1], c.interval[2]), c.chromosome_size)) for _ in 1:c.population_size])
+    return Population([IntegerChromosome([rand(c.intervals[i][1]:c.intervals[i][2]) for i in 1:c.chromosome_size]) for _ in 1:c.population_size])
 end
+
 
 
 export RealUniformInitialization
