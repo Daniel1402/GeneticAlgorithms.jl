d69e33350bd0a30e6fe132e2f3d9dc83226a3996
Dominik S. <dsiebelt@campus.tu-berlin.de>
2025-01-15 12:39:42 +0100

    restore runtests

---
+++ test/runtests.jl
@@ -0,0 +1,16 @@
+using Test
+
+#include("../src/GeneticAlgorithms.jl")
+using GeneticAlgorithms
+
+include("test_crossover.jl")
+include("test_population_initialization.jl")
+include("test_selection.jl")
+include("test_mutation.jl")
+include("test_fitness.jl")
+include("test_genetic_algorithms.jl")
+include("test_utils.jl")
+
+@testset "GeneticAlgorithms.jl" begin
+    # Write your tests here.
+end
