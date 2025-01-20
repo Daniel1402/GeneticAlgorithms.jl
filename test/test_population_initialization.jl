aba0caa188d4d9fd550556ec7322b157c23689a7
Fabian <59339595+DesjardinsRepos@users.noreply.github.com>
2025-01-20 13:20:02 +0100

    support non-uniform initialization (#34)
    * support non-uniform initialization
    
    * add additional testsets
    
    * remove images

--- test/test_population_initialization.jl
+++ test/test_population_initialization.jl
@@ -21,5 +21,17 @@ using GeneticAlgorithms.PopulationInitialization
     @test_throws ArgumentError RealUniformInitialization(0, -1, (0.0, 1.0))
     @test_throws ArgumentError RealUniformInitialization(3, 1, (2, 1))
     @test_throws ArgumentError RealUniformInitialization(3, 1, (1, 1))
+end
 
+@testset "AsymmetricPopulationInitialization" begin
+    population_initialization = RealUniformInitialization(10, 3, [(0.0, 1.0), (-10.0, 10.0), (5.0, 15.0)])
+    population = population_initialization()
+    @test length(population.chromosomes) == 10
+    @test all([length(population.chromosomes[i].genes) == 3 for i in 1:10])
+    @test all([chromosome.genes[1] >= 0.0 && chromosome.genes[1] <= 1.0 &&
+            chromosome.genes[2] >= -10.0 && chromosome.genes[2] <= 10.0 &&
+            chromosome.genes[3] >= 5.0 && chromosome.genes[3] <= 15.0 for chromosome in population.chromosomes])
+
+    @test_throws ArgumentError RealUniformInitialization(10, 3, [(0.0, 1.0), (-10.0, 10.0)])
+    @test_throws ArgumentError RealUniformInitialization(10, 2, (1.0, 1.0))
 end
