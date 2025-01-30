87be50dd69f7262049cec067f619f56b5f803392
Daniel Kirste <daniel.kirste@stud.tu-darmstadt.de>
2025-01-30 19:34:01 +0100

    Fix overwritten function warning

--- test/test_visualization.jl
+++ test/test_visualization.jl
@@ -3,15 +3,15 @@ using GeneticAlgorithms.Visualization
 
 @testset "Visualization Tests" begin
     # Sample test
-    f(x, y) = sin(x) * cos(y)
+    f(x::Float64, y::Float64) = sin(x) * cos(y)
     path = [(-π / 2, 0.0), (π / 2, 0.0)]
     plt = visualize_function_with_contours(f, x_range=(-1π, 1π), y_range=(-1π, 1π), path=path)
     savefig(plt, "sin_times_cos.png")
 
     # Rosenbrock
-    f(x, y) = rosenbrock_fitness(Chromosome([x, y]))
+    g(x::Float64, y::Float64) = rosenbrock_fitness(Chromosome([x, y]))
     path = [(1.0, 1.0)]
-    plt = visualize_function_with_contours(f, x_range=(-2.0, 2.0), y_range=(-1.0, 3.0), path=path)
+    plt = visualize_function_with_contours(g, x_range=(-2.0, 2.0), y_range=(-1.0, 3.0), path=path)
     savefig(plt, "rosenbrock.png")
 
     path = [Chromosome([0.0, 0.0]), Chromosome([0.5, 0.5]), Chromosome([1.0, 1.0])]
