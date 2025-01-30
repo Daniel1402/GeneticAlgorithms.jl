2cfd9030735b9d8acf4724834bbafbca20920f92
Daniel Kirste <daniel.kirste@stud.tu-darmstadt.de>
2025-01-30 20:42:34 +0100

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,24 +2,24 @@ using Test
 
 using GeneticAlgorithms
 
-# @testset "Crossover" begin
-#     include("test_crossover.jl")
-# end
-# @testset "PopulationInitialization" begin
-#     include("test_population_initialization.jl")
-# end
-# @testset "Selection" begin
-#     include("test_selection.jl")
-# end
-# @testset "Mutation" begin
-#     include("test_mutation.jl")
-# end
-# @testset "Fitness" begin
-#     include("test_fitness.jl")
-# end
+@testset "Crossover" begin
+    include("test_crossover.jl")
+end
+@testset "PopulationInitialization" begin
+    include("test_population_initialization.jl")
+end
+@testset "Selection" begin
+    include("test_selection.jl")
+end
+@testset "Mutation" begin
+    include("test_mutation.jl")
+end
+@testset "Fitness" begin
+    include("test_fitness.jl")
+end
 @testset "Optimization" begin
     include("test_optimization.jl")
 end
-# @testset "Visualization" begin
-#     include("test_visualization.jl")
-# end
+@testset "Visualization" begin
+    include("test_visualization.jl")
+end
